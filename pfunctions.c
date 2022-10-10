94799a1d336e79a41c4407647c7c28e9dd6b4972
Roia Langa Tembo <roia@kali.theitmogul.co.za>
2022-10-10 20:47:16 +0200

    pointers in functions

--- pfunctions.c
+++ pfunctions.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+void minus10(int *v);
+
 int main() 
 {
-    printf("This is the way\n");
-    printf("totally fine");
+    int value = 100;
+    printf("Value is %d\n",value);
+    minus10(&value);
+    printf("Value is %d\n",value);
 
     return 0;
+}
+
+void minus10(int *v)
+{
+    *v = *v - 10;
 }
