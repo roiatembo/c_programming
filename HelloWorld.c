a8ee435ac9c3f170eecf2f31004176eb9fd7c2ad
Roia Tembo <roia@itmogul.itmogul.co.za>
2024-02-19 22:01:01 +0200

    data types

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
+    int x;
+    x = 123;
+    float price = 2.23;
+    char grade = 'c';
+    char name[] = "Roia";
     printf("I like pizza\n");
     printf("Its really good");
     return 0;
